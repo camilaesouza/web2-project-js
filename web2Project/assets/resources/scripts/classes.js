18a6833d4f17ab4d7a6687df542bc65a01b2aea7
Camila <camila-emanuele@hotmail.com>
2019-07-03 20:34:27 -0300

    Adiiconando o projeto

--- web2Project/assets/resources/scripts/classes.js
+++ web2Project/assets/resources/scripts/classes.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-    $( ".card-img" ).on( "click", function() {
-        if( $(this).attr('id') == 'image-class') {
-         $(this).attr("src", "assets/resources/img/Cartas/card-"+$(this).attr('value')+".png")
-         $(this).attr('id', 'image-character') 
+$(document).ready(function () {
+    $(".card-img").on("click", function () {
+        if ($(this).attr('id') == 'image-class') {
+            $(this).attr("src", "assets/resources/img/Cartas/card-" + $(this).attr('value') + ".png")
+            $(this).attr('id', 'image-character')
         } else {
-            $(this).attr("src", "assets/resources/img/Cartas/class-"+$(this).attr('value')+".png")
-            $(this).attr('id', 'image-class') 
+            $(this).attr("src", "assets/resources/img/Cartas/class-" + $(this).attr('value') + ".png")
+            $(this).attr('id', 'image-class')
         }
     });
 });
--- web2Project/assets/resources/scripts/forms.js
+++ web2Project/assets/resources/scripts/forms.js
@@ -1,90 +1,100 @@
+window.addEventListener("load", function (event) {
+  (function () {
+    $(document).ready(function () { 
+      $('.phone_with_ddd').mask('(00) 0000-0000');
 
-window.addEventListener("load", function(event) {
-(function(){
-    let Person = function(name, lastName, phone, email, notifications, message){
+     });
+
+     function muda(){
+      document.getElementById('text-class').style.color="#ffff00";
+    }
+
+    let Person = function (name, lastName, phone, email, notifications, message) {
       this.name = name;
       this.lastName = lastName;
-    	this.phone = phone;
-    	this.email = email;
-    	this.notifications = notifications;
-    	this.message = message;
+      this.phone = phone;
+      this.email = email;
+      this.notifications = notifications;
+      this.message = message;
     }
 
     let personArray = [];
 
-   addPerson = () => {
-    document.getElementById('form-contact').onsubmit = function(e){
-    	e.preventDefault();
-    	let name = $('#name').val();
-    	let lastName = $('#last-name').val();
-    	let phone = $('#phone').val();
+    addPerson = () => {
+      document.getElementById('form-contact').onsubmit = function (e) {
+        e.preventDefault();
+        let name = $('#name').val();
+        let lastName = $('#last-name').val();
+        let phone = $('#phone').val();
         let email = $('#email').val();
-        let notifications= document.getElementsByName('notifications');
-        for (var i=0; i < notifications.length; i++){ 
-            if (notifications[i].checked){ 
-                alert('Em breve você receberá mais notificações sobre o projeto!');
-            }
+        let notifications = document.getElementsByName('notifications');
+        for (var i = 0; i < notifications.length; i++) {
+          if (notifications[i].checked) {
+            alert('Soon you will receive more notifications about the project!');
+          }
         }
-      
-    	let message = $('#message').val();
-    	let person = new Person(name, lastName, phone, email, notifications, message);
+
+        let message = $('#message').val();
+        let person = new Person(name, lastName, phone, email, notifications, message);
 
         personArray.push(person);
         var array = personArray.slice();
 
         confirm('Are you sure you want to submit the form, ' + array[0].name + '?');
-        if(confirm){
+        if (confirm) {
           nickname = prompt("How would you like to be called?")
           alert('Thank you for registering: ' + nickname);
         }
+      }
     }
-  }
 
-  addPerson();
+    addPerson();
 
-    slideDownToform =  function(){
+    slideDownToform = function () {
 
-    $("#form-contact").hide();
+      $("#form-contact").hide();
 
-    $("#feedback").click(function(){
+      $("#feedback").click(function () {
         $("#form-contact").slideToggle("slow");
-        $( "#name" ).focus();
-        $( "#inputName" ).blur();
+        $("#name").focus();
+        $("#inputName").blur();
       })
-      
+
       $("#showJoinTeam").hide();
-      
-     $("#joinTeam").click(function(){
+
+      $("#joinTeam").click(function () {
         $("#showJoinTeam").slideToggle("slow");
-        $( "#inputName" ).focus();
-        $( "#name" ).blur();
-    });
+        $("#inputName").focus();
+        $("#name").blur();
+      });
     }
 
     slideDownToform();
 
     userFormId = document.getElementById('save');
-    userFormId.addEventListener("click", function(){
-      var arrayUser=[]
-        if(JSON.parse(localStorage.getItem("user")) != null){
-          arrayUser.push(JSON.parse(localStorage.getItem("user")));
-        }
-        var name = document.getElementById("inputName").value;
-      
-        var user = {name: name};
-        arrayUser.push(user);
-        var userJson = JSON.stringify(arrayUser);
-        localStorage.setItem("Users", userJson);
+    userFormId.addEventListener("click", function () {
+      var arrayUser = []
+      if (JSON.parse(localStorage.getItem("user")) != null) {
+        arrayUser.push(JSON.parse(localStorage.getItem("user")));
+      }
+      var name = document.getElementById("inputName").value;
 
-        var numberOfNames = arrayUser.length;
+      var user = {
+        name: name
+      };
+      arrayUser.push(user);
+      var userJson = JSON.stringify(arrayUser);
+      localStorage.setItem("Users", userJson);
 
-        getUser = function(userName){
-          return alert(`thank you for the send your interest to join us, ${userName}`);
-        }
-        getUser(user.name);
+      var numberOfNames = arrayUser.length;
 
-        alert(`We have ${numberOfNames} interest to join us, is funny`);
-    }); 
+      getUser = function (userName) {
+        return alert(`thank you for the send your interest to join us, ${userName}`);
+      }
+      getUser(user.name);
+
+      alert(`We have ${numberOfNames} interest to join us, is funny`);
+    });
 
   })();
 });
